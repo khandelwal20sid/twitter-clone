8c1aa1725f2ffc22147b34d06729760cd417ab09
siddharth <khandelwal20sid@gmail.com>
2021-06-30 01:59:29 +0530

    Rightbar structure

--- src/components/RightBar.js
+++ src/components/RightBar.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./RightBar.css"
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/Search";
 
 function RightBar() {
-    return (
-        <div className="rightBar">
-            <h1>Right Bar</h1>
-        </div>
-    )
+  return (
+    <div className="rightBar">
+      <div className="rigthBar_input">
+        <SearchIcon className="rightBar_searchIcon" />
+        <input placeholder="Search Tweet" type="text" />
+      </div>
+
+      <div className="rightBar_container">
+        <h2>What's happening</h2>
+      
+      <TwitterTweetEmbed tweetId={"1337808745695485953"} />
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="saurabhnemade"
+        options={{ height: 400 }}
+      />
+      <TwitterShareButton
+        url={"https://facebook.com/saurabhnemade"}
+        options={{ text: "#reactjs is awesome", via: "saurabhnemade" }}
+      />
+      </div>
+    </div>
+  );
 }
 
-export default RightBar
+export default RightBar;
