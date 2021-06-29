af3b857e10c4ffe4e1e1b7d12a96737cdde744bc
siddharth <khandelwal20sid@gmail.com>
2021-06-30 01:20:32 +0530

    feed structure

---
+++ src/components/Post.js
@@ -0,0 +1,45 @@
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./Post.css";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import PublishIcon from "@material-ui/icons/Publish";
+
+function Post({ diplayName, username, verified, text, image, avatar }) {
+  return (
+    <div className="post">
+      <div className="post_avatar">
+        <Avatar src="/assets/avatar.svg" />
+      </div>
+      <div className="post_body">
+        <div className="post_header">
+          <div className="post_header_text">
+            <h3>
+              Siddharth Khandelwal <VerifiedUserIcon className="post_badge" />{" "}
+              <span className="post_header_username">
+                @khandelwal20sid
+              </span>{" "}
+            </h3>
+          </div>
+          <div className="post_header_description">
+            <p>My first tweet</p>
+          </div>
+        </div>
+        <img
+          src="https://i.pinimg.com/236x/1b/55/07/1b55075a3695107d4e4c86d8aaef9cd0.jpg"
+          alt=""
+        />
+        <div className="post_footer">
+          <ChatBubbleOutlineIcon fontSize="small" />
+          <RepeatIcon fontSize="small" />
+          <FavoriteBorderIcon fontSize="small" />
+          <PublishIcon fontSize="small" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
